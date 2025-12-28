3797bd301c033ca5e64f3790f8d12fbbdd6b5d3d
eunseo0903 <alice5808@naver.com>
2025-12-28 23:19:28 +0900

    [feat] /login 페이지로 라우팅 #4

--- deartime/src/App.jsx
+++ deartime/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import Landing from "./pages/landing.jsx";
+import Login from "./pages/login.jsx";
+import Home from "./pages/home.jsx";
 
 function App() {
-  return <Landing />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- deartime/src/pages/landing.jsx
+++ deartime/src/pages/landing.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import landingImg from "../assets/landing.svg";
 import logoImg from "../assets/logo.svg";
 import "../styles/landing.css";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-    const handleGoogleLogin = () => {
-        console.log("Google login clicked");
-        // 구글 OAuth 연결
-    };
+    const navigate = useNavigate();
 
     return (
         <div className="landing-container">
@@ -16,15 +14,12 @@ const Landing = () => {
             <p className="landing-text">
             별빛처럼 사라지지 않는 기억을 기록하는 곳
             </p>
-            <button className="google-login-btn" onClick={handleGoogleLogin}>
-                <img
-          src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-          alt="google"
-        />
-        Google 계정으로 로그인
-      </button>
-    </div>
-  );
+            <button className="google-login-btn" onClick={() => navigate("/login")}>
+                <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="google"/>
+                Google 계정으로 로그인
+            </button>
+        </div>
+    );
 };
 
 export default Landing;
