04aff210f929c1e920d25b59357616e3ce425bce
eunseo0903 <alice5808@naver.com>
2025-12-29 02:35:36 +0900

    [fix] 랜딩 페이지를 로그인 페이지로 수정 #1

--- deartime/src/pages/login.jsx
+++ deartime/src/pages/login.jsx
@@ -1,14 +1,25 @@
-import { useGoogleLogin } from "@react-oauth/google";
+import React, { useEffect } from "react";
+import backgroundImg from "../assets/background.svg";
+import logoImg from "../assets/logo.svg";
+import "../styles/landing.css";
 import { useNavigate } from "react-router-dom";
+import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
 
+  // 이미 로그인된 유저면 홈으로
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      navigate("/home");
+    }
+  }, [navigate]);
+
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       try {
-        // 백으로 구글 엑세스 토큰 전달
         const res = await axios.post(
           "http://localhost:8080/auth/google",
           {
@@ -16,13 +27,17 @@ const Login = () => {
           }
         );
 
-        // 백에서 받은 JWT 토큰을 로컬 스토리지에 저장
-        localStorage.setItem("accessToken", res.data.accessToken);
+        const { isNewUser, accessToken } = res.data;
 
-        // 로그인 성공 시 회원가입페이지로 이동
-        navigate("/signup");
+        localStorage.setItem("accessToken", accessToken);
+
+        if (isNewUser) {
+          navigate("/signup");
+        } else {
+          navigate("/home");
+        }
       } catch (err) {
-        console.error("로그인 실패", err);
+        console.error("Google 로그인 실패", err);
       }
     },
     onError: () => {
@@ -31,10 +46,19 @@ const Login = () => {
   });
 
   return (
-    <div className="login-container">
-      <h2>로그인</h2>
+    <div className="landing-container">
+      <img src={backgroundImg} alt="landing" className="background-img" />
+      <img src={logoImg} alt="logo" className="logo-img" />
+
+      <p className="landing-text">
+        별빛처럼 사라지지 않는 기억을 기록하는 곳
+      </p>
 
       <button className="google-login-btn" onClick={googleLogin}>
+        <img
+          src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+          alt="google"
+        />
         Google 계정으로 로그인
       </button>
     </div>
