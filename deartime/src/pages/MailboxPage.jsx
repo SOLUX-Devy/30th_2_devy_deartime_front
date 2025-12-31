23728986c2f9b47c3ae029cf39f8831f3aee2e21
nylee16 <nylee16@sookmyung.ac.kr>
2025-12-31 18:25:05 +0900

    feature : 우체통 페이지 UI 기본 틀 및 구조 1차 구현 (#2)

--- deartime/src/pages/MailboxPage.jsx
+++ deartime/src/pages/MailboxPage.jsx
@@ -1,3 +1,50 @@
-//안녕하세요
-console.log("test");
-console.log("test2");
+import React from 'react';
+import '../styles/MailboxPage.css';
+import LetterCard from '../components/LetterCard';
+import MailTabs from '../components/MailTabs'; 
+import SendButton from '../components/SendButton'; 
+
+// API에서 받은 실제 JSON 구조를 MOCK 데이터로 사용
+const apiResponse = { 
+    data: {
+        data: [
+        {
+            letterId: 1,
+            senderNickname: "테스터_A",
+            title: "첫 편지입니다",
+            summary: "17148",
+            themeCode: "DEFAULT",
+            sentAt: "2025-12-15T00:32:08.164649",
+            isRead: true,
+            isBookmarked: true
+        }
+        ]
+    }
+};
+
+const MailboxPage = () => {
+    const letters = apiResponse.data.data;
+
+    return (
+        <div className="mailbox-container">
+        <div className="main-navbar-placeholder">Main Navbar</div>
+        
+            <div className="mailbox-content">
+                <header className="mailbox-header">
+                    {/* Tabs, Button 컴포넌트 삽입 */}
+                    <MailTabs />
+                    <SendButton />
+                </header>
+
+                <main className="letter-grid">
+                    {letters.map((letter) => (
+                        <LetterCard key={letter.letterId} data={letter} />
+                    ))}
+                </main>
+            </div>
+
+        </div>
+    );
+};
+
+export default MailboxPage;
