10fb99d5baf958fac59098d72a50fb9183ebb375
hycho04 <hycho8901@gmail.com>
2026-01-03 16:59:41 +0900

    Design: update header UI

--- deartime/src/App.jsx
+++ deartime/src/App.jsx
@@ -5,12 +5,13 @@ import Header from "./components/Header";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
 import Home from "./pages/home.jsx";
+import TimeCapsule from "./pages/timecapsule";
 
 /*개발 후 주석 풀어서 사용해주세요! -나연 */
 /*import Gallery from "./pages/gallery";*/
 import Letterbox from "./pages/letterboxPage";
-/*import Timecapsule from "./pages/timecapsule";
-import Freind from "./pages/freind"; */
+import Timecapsule from "./pages/timecapsule";
+/*import Freind from "./pages/freind"; */
 
 export default function App() {
   return (
@@ -24,7 +25,7 @@ export default function App() {
 
         {/* <Route path="/gallery" element={<Gallery />} /> */}
         <Route path="/letterbox" element={<Letterbox />} />
-        {/* <Route path="/timecapsule" element={<Timecapsule />} /> */}
+        <Route path="/timecapsule" element={<Timecapsule />} />
         {/* <Route path="/freind" element={<Freind />} /> */}
         
       </Routes>
