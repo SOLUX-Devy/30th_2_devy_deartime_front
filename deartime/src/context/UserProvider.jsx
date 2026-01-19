f0e6cd95382f18da45290cdc351049b6ed0c22a4
eunseo0903 <alice5808@naver.com>
2026-01-19 13:24:39 +0900

    [add] import 문 추가

--- deartime/src/context/UserProvider.jsx
+++ deartime/src/context/UserProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { UserContext } from "./UserContext";
 
 export default function UserProvider({ children }) {
   const [user, setUser] = useState(null);
